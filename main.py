6c48b2bbdc43482b45eaa39b98d0cafdddd2900e
Francisco J. Castellanos <fcastellanos@dlsi.ua.es>
2023-12-12 11:39:15 +0000

    Deleting ; in test report

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
       str_properties += str(config.n_pa) + separator
       str_properties += str(config.ink_rate).replace(".",",") + separator  
       str_result = str_properties+separator
-      str_result += separator + "VAL"+separator
+      str_result += "VAL"+separator
       str_result += str(best_th_val).replace(".",",") + separator
       str_result += number_to_string(best_fm_val) + separator
       str_result += number_to_string(prec_val) + separator
